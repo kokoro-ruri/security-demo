3e5e7cb6e7f05afea994e1416319203d4dda0593
Yaoyao <1058578628@qq.com>
2020-07-07 15:34:57 +0800

    解决druid连接池和mp的动态数据源冲突

--- src/main/java/securityDemo/MyApplication.java
+++ src/main/java/securityDemo/MyApplication.java
@@ -1,12 +1,13 @@
 package securityDemo;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 //@SpringBootApplication()
 @MapperScan("securityDemo.mapper")
 public class MyApplication {
